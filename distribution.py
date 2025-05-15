9a12b25d5164c49ab8c01ee7b5d45fe86c6059ff
jlapeyra <jlapeyraa@gmail.com>
2025-05-15 16:32:32 +0200

    working version

--- distribution.py
+++ distribution.py
@@ -6,9 +6,11 @@ import numpy as np
 from utils import id, windowed, key_list
 from itertools import chain, combinations
 from abc import ABC, abstractmethod
+import random
+from tqdm import tqdm
 
-
-ALPHA = 0.1
+ALPHA = 0.01
+NULL = '---'
 
 class Distribution(Counter):
     # Counter allows __add__
@@ -27,6 +29,33 @@ class Distribution(Counter):
     
     def logProbabilityDistribution(self, alpha:float=ALPHA, num_keys:int=None):
         return Distribution({key: self.logProbability(key, alpha, num_keys) for key in self.keys()})
+    
+    prob_array:np.ndarray = None
+    __choices = None
+
+    def randomChoice(self, precompute=10):
+        assert not self.empty()
+        if self.__choices:
+            return self.__choices.pop()
+        if self.prob_array is None:
+            self.prob_array = np.array([self.probability(k) for k in self.keys()])
+        assert precompute >= 1
+        self.__choices = list(np.random.choice(list(self.keys()), size=precompute, p=self.prob_array))
+        if '' in self.__choices:
+            pass
+        return self.__choices.pop()
+
+    
+    def empty(self):
+        return len(self) == 0
+    
+    __total = None
+    def total(self):
+        if self.__total is None:
+            self.__total = super().total()
+        return self.__total
+
+
 
 
 class ConditionalDistribution(defaultdict[object, Distribution]):
@@ -60,38 +89,56 @@ class ConditionalDistribution(defaultdict[object, Distribution]):
 class NGram(ConditionalDistribution):
     # Counter allows __add__
 
-    def __init__(self, n:int, default=None, func_prior:Callable=id, func_posterior:Callable=id, func:Callable=None):
+    def __init__(self, n:int, null=NULL): #, func_prior:Callable=id, func_posterior:Callable=id, func:Callable=None):
         self.n = n
-        self.default = default
-        if func is not None:
-            self.func_posterior = func
-            self.func_prior = func
-        else:
-            self.func_prior = func_prior
-            self.func_posterior = func_posterior
+        self.null = null
+        # if func is not None:
+        #     self.func_posterior = func
+        #     self.func_prior = func
+        # else:
+        #     self.func_prior = func_prior
+        #     self.func_posterior = func_posterior
         super().__init__()
 
     def __sliding_window(self, sequence:Sequence):
-        head, tail = [self.default]*(self.n-1), [self.default]
-        prior     = map(self.func_prior, sequence)
-        posterior = map(self.func_posterior, sequence)
-        prior     = chain(head, prior)
-        posterior = chain(posterior, tail)
+        # prior     = map(self.func_prior, sequence)
+        # posterior = map(self.func_posterior, sequence)
+        # prior     = list(chain(head, prior))
+        # posterior = list(chain(posterior, tail))
+        head, tail = [self.null]*(self.n-1), [self.null]
+        sequence = list(sequence)
+        prior = head + sequence
+        posterior = sequence + tail
         return zip(windowed(prior, self.n-1), posterior)
 
     def feed(self, sequence:Sequence):
+        if self.n == 1:
+            pass
         for k1, k2 in self.__sliding_window(sequence):
-            self[k1][k2]
+            assert k2 != '', sequence
+            self[k1][k2] += 1
         return self
     
+    def flat(self):
+        ret = {}
+        for prior, dist in self.items():
+            for posterior, count in dist.items():
+                ret[prior, posterior] = count
+        return ret
+    
     def save(self, filename):
+        '''
+        Save the model
+        '''
         with open(filename, 'w', encoding='utf-8') as f:
-            for (prior, posterior), count in sorted(self.items()):
+            for (prior, posterior), count in sorted(self.flat().items()):
                 print(*prior, posterior, count, file=f)
                 
     def load(self, filename, reverse=False):
-        with open(filename, 'w', encoding='utf-8') as f:
-            for line in f:
+        if self.n == 1:
+            pass
+        with open(filename, 'r', encoding='utf-8') as f:
+            for line in tqdm(f.readlines()):
                 try:
                     *keys, count = line.strip('\n').split()
                     if reverse:
@@ -102,59 +149,80 @@ class NGram(ConditionalDistribution):
                 except:
                     raise Exception(f'Wrong format: expected {self.n} keys and a number. Got: "{line}"')
                 else:
-                    prior     = tuple(map(self.func_prior, prior))
-                    posterior = self.func_posterior(posterior)
-                    self[prior, posterior] += count
+                    #prior     = tuple(map(self.func_prior, prior))
+                    #posterior = self.func_posterior(posterior)
+                    self[tuple(prior)][posterior] += count
         return self
     
-    def generate():
+
+class MetaNGram(list[NGram]):
+    ngrams : list[NGram]
+
+    def __init__(self, n):
+        self.ngrams = []
+        for i in range(1, n+1):
+            self.ngrams.append(NGram(i))
+
+    def __getitem__(self, key) -> NGram:
+        return self.ngrams[key]
+
+    def __iter__(self):
+        return iter(self.ngrams)
+    
+    def __len__(self):
+        return super().__len__()
+
+    def generate(self, size):
+        max_prior = len(self.ngrams)
+        INIT = tuple([NULL]*max_prior) 
+        prior = INIT
+        words = []
+        for _ in range(size):
+            for ngram in reversed(self.ngrams):
+                prior_size = ngram.n - 1
+                distrib = ngram[prior[max_prior-prior_size:]]
+                if distrib.total() > random.randint(1, 3):
+                    break
+            word = distrib.randomChoice()
+            if word == NULL:
+                prior = INIT
+            else:
+                prior = prior[1:] + (word,)
+            words.append(word)
+            print((word if word!='\n' else repr(word)).ljust(16), ngram.n, distrib.total())
+        print(*words)
+
+    def feed(self, sequence: Sequence):
+        for ngram in self.ngrams:
+            ngram.feed(sequence)
+        return self
+
+    def load(self, filename_pattern:str, reverse=False):
+        for ngram in self.ngrams:
+            ngram.load(filename_pattern.format(n=ngram.n), reverse=reverse)
+        return self
+
+    def save(self, filename_pattern:str):
+        for ngram in self.ngrams:
+            ngram.save(filename_pattern.format(n=ngram.n))
         
 
-    
 
-    
 
-class CoOccurrences(ConditionalDistribution):
-    '''
-    Co-occurrences of pairs of elements in windows of size n
-    '''
-    def __init__(self, n:int, func:Callable=id) -> None:
-        assert n >= 2
-        self.n = n
-        self.func = func
-        super().__init__()
+if __name__ == '__main__':
+    import glob
+    data_fn = rf'C:\Users\Usuari\Desktop\MIRI\Q2\IQL\IQL-lab1\europarl\data-v7\raw\europarl-v7.ca-en.ca'
 
-    def __sliding_window(self, sequence:Sequence, n):
-        return windowed(map(self.func, sequence), n)
-    
-    def feed(self, sequence:Sequence):
-        for elems in self.__sliding_window(sequence, self.n):
-            for x, y in combinations(elems, 2):
-                self[x][y] += 1
-                self[y][x] += 1
-        return self
+    model = MetaNGram(n=5)
+    for fn in glob.glob(rf"C:\Users\Usuari\Desktop\Documents\prolliure\hamilton\lletres\*"):
+        with open(fn, 'r', encoding='utf-8') as f:
+            model.feed(f.read().split())
+            model.save("{}.{}.{}-gram.model".format('hamilton', 'ca', '{n}'))
 
-    def save(self, filename):
-        saved_keys = set()
-        with open(filename, 'w', encoding='utf-8') as f:
-            for (key1, key2), count in sorted(self.items()):
-                if (key2, key1) not in saved_keys:
-                    print(key1, key2, count, file=f)
-                    saved_keys.add((key1, key2))
+    model = MetaNGram(n=5)
+    model.load("hamilton.ca.{n}-gram.model")
+    model.generate(100)
 
-    def load(self, filename):
-        with open(filename, 'r', encoding='utf-8') as f:
-            for line in f:
-                try:
-                    key1, key2, count = line.strip('\n').split()
-                    count = int(count)
-                except:
-                    raise Exception(f'Wrong format: expected `key1 key2 num`. Got: "{line}"')
-                else:
-                    self[key1, key2] += count
-        return self
-    
 
-if __name__ == "__main__":
-    print(CoOccurrences(3).feed('abc'))
+        
 
